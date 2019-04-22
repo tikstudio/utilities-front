b3a620245161aadc5cb2a31d7e184164577f4a3b
Tea <tea@tea.77>
2019-04-22 18:38:36 +0400

    first

---
+++ src/data.js
@@ -0,0 +1,21 @@
+const data = [
+    {
+        name: 'Pashik',
+        surname: 'Poxosyan',
+        MName: 'Gvidoni',
+        address: 'Timiryazev',
+    },
+    {
+        name: 'Hayk',
+        surname: 'Ghevondyan',
+        MName: 'Garniki',
+        address: 'Sovxoz',
+    },
+    {
+        name: 'Tatul',
+        surname: 'Avoyan',
+        MName: 'Karleni',
+        address: 'Stroymash',
+    },
+]
+export default data
