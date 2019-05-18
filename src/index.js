49670bb6184ea0cd0c6ccc284c3b36bad1aab4c5
haykbernetsyan <haykbernetsyan@mail.ru>
2019-05-18 22:30:00 +0400

    meneger last

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './Assets/stylus/manager.css'
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware} from 'redux'
 import * as serviceWorker from './serviceWorker';
--- src/pages/Manager.js
+++ src/pages/Manager.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-// import data from '../data'
 import {manager} from "../store/actions/manager";
 import {connect} from "react-redux";
+import TextField from '@material-ui/core/TextField';
 
 class Manager extends Component {
     constructor(props) {
@@ -38,34 +38,38 @@ class Manager extends Component {
 
     render() {
         console.log(this.props.peoples);
-        // {
-        //     const people =  data.map((value) => {
-        //         if (this.state.name !== null && this.state.surname !== null && this.state.mName !== null){
-        //             if (this.state.name === value.name && this.state.surname === value.surname && this.state.mName === value.mName) {
-        //                 return true
-        //             } else {
-        //                 return false
-        //             }
-        //         }
-        //
-        //     })
-        //
-        // }
         return (
-          <div>
-              <h2>Manager</h2>
-              <br/>
-              <input type="text" onBlur={(e) => this.inputs(e, 'name')} placeholder="name"/>
-              <br/>
-              <input type="text" onBlur={(e) => this.inputs(e, 'lName')} placeholder="lName"/>
-              <br/>
-              <input type="text" onBlur={(e) => this.inputs(e, 'mName')} placeholder="mName"/>
-              <br/>
-              <button onClick={this.send}>save</button>
-          </div>
+            <div className='background'>
+                <h2 className="manager">Manager</h2>
+                <br/>
+                <div className='inputs'>
+                    <label htmlFor="uName">Name</label>
+                    <br/>
+                    <TextField type='text' id="uName" onBlur={(e) => this.inputs(e, 'name')} placeholder="Name"/>
+                </div>
+                <br/>
+                <div className='inputs'>
+                    <label htmlFor="uName">lName</label>
+                    <br/>
+                    <TextField type='text' id="lName" onBlur={(e) => this.inputs(e, 'lName')} placeholder="lName"/>
+                </div>
+                <br/>
+                <div className='inputs'>
+                    <label htmlFor="uName">mName</label>
+                    <br/>
+                    <TextField type='text' id="mName" onBlur={(e) => this.inputs(e, 'mName')} placeholder="mName"/>
+                </div>
+                <br/>
+                <button className="button"  onClick={this.send}>
+                    send
+                    <div className="button__horizontal"/>
+                    <div className="button__vertical"/>
+                </button>
+            </div>
         );
     }
 }
+
 const mapStateToProps = (state) => ({
     peoples: state.manager.peoples,
 });
@@ -75,8 +79,8 @@ const mapDispatchToProps = {
 };
 
 const ManagerContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps,
+    mapStateToProps,
+    mapDispatchToProps,
 )(Manager);
 
 export default ManagerContainer;
