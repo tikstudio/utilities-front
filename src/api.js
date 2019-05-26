abbff1f4debc904e9e88f54fece5854780eaac0e
Tigran Muradyan <tigran.inbox@gmail.com>
2019-05-26 16:24:50 +0400

    new API file

---
+++ src/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { stringify as qs } from 'querystringify';
+
+const API_URL = 'http://localhost:5000';
+
+axios.defaults.baseURL = API_URL;
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers['Authorization'] = token;
+  }
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+export function login(data) {
+  return axios.post("/users/login", {
+    username: data.email,
+    password: data.password,
+  })
+}
+
+export function getPeoples(page) {
+  const query = qs({
+    page: page
+  });
+  return axios.get(`/peoples?${query}`)
+}
