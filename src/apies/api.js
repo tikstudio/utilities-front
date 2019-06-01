67bf67e04663e625401783eedb2b2241f3fc19ee
Artur <www.artur.artur@mail.ru>
2019-06-01 11:38:06 +0400

    UI

--- src/apies/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios/index";
-
-const API_URL = 'http://localhost:5000';
-
-export function signIn(data) {
-    return axios.post(API_URL + "/signIn",{data})
-}
--- src/apies/saveUsersApi.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios/index";
-
-const API_URL = 'http://localhost:5000';
-
-export function saveUsersApi(data) {
-    return axios.post(API_URL + "/save-users",{data})
-}
